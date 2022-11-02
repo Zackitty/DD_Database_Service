3e8e30dd206ce49253160c71c6a64540f828a16d
zackitty <theelfenliedpictureshow@yahoo.com>
2022-11-02 13:30:46 -0500

    init

---
+++ src/main/java/com/zackitty/DD_Database_Service/DdDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_Database_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zackitty/DD_Database_Service/DdDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_Database_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
